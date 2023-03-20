6b84cadd16431c2404d0e61330fc290a9ffd7a41
Thirdy <gayaresthird@gmail.com>
2023-03-20 13:05:05 +0800

    final final

--- src/Two.java
+++ src/Two.java
@@ -3,10 +3,7 @@ import java.util.Scanner;
 public class Two {
 
     public static void main(String[] args) {
-
-
         int num1,num2;
-
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter first number:");
         num1=sc.nextInt();
